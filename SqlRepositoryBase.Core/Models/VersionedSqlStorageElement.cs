7ba5749c74fd5eede0ccc5acb34d1ee46a6cddb4
Igor Chernyshev <sonic96.ic@gmail.com>
2023-02-26 00:56:53 +0500

    lets try like this

--- SqlRepositoryBase.Core/Models/VersionedSqlStorageElement.cs
+++ SqlRepositoryBase.Core/Models/VersionedSqlStorageElement.cs
@@ -1,6 +1,9 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace SqlRepositoryBase.Core.Models;
 
 public class VersionedSqlStorageElement : SqlStorageElement
 {
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public long Version { get; set; }
 }
