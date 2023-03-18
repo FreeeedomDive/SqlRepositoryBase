0ae58860ffbcf90f65760b36c831ce7544baf8cc
Igor Chernyshev <sonic96.ic@gmail.com>
2023-03-18 12:57:59 +0500

    configure repositories with versions

--- SqlRepositoryBase.Configuration/Extensions/AspNetServiceCollectionExtensions.cs
+++ SqlRepositoryBase.Configuration/Extensions/AspNetServiceCollectionExtensions.cs
@@ -8,15 +8,34 @@ public static class AspNetServiceCollectionExtensions
 {
     public static IServiceCollection ConfigurePostgreSql(this IServiceCollection services)
     {
-        var allTypes = AppDomain.CurrentDomain.GetAssemblies().SelectMany(s => s.GetTypes()).ToArray();
+        var types = AppDomain.CurrentDomain.GetAssemblies().SelectMany(s => s.GetTypes()).ToArray();
+        return services.ConfigureCommonRepositories(types).ConfigureVersionedRepositories(types);
+    }
 
-        var sqlStorageElementTypes = allTypes
+    private static IServiceCollection ConfigureCommonRepositories(this IServiceCollection services, IEnumerable<Type> types)
+    {
+        var sqlStorageElementTypes = types
             .Where(p => typeof(SqlStorageElement).IsAssignableFrom(p))
             .Where(p => p != typeof(SqlStorageElement));
         foreach (var sqlStorageElementType in sqlStorageElementTypes)
         {
             var genericSqlRepositoryInterfaceType = typeof(ISqlRepository<>).MakeGenericType(sqlStorageElementType);
             var genericSqlRepositoryImplementationType = typeof(SqlRepository<>).MakeGenericType(sqlStorageElementType);
+            services.AddTransient(genericSqlRepositoryInterfaceType, genericSqlRepositoryImplementationType);
+        }
+
+        return services;
+    }
+
+    private static IServiceCollection ConfigureVersionedRepositories(this IServiceCollection services, IEnumerable<Type> types)
+    {
+        var sqlStorageElementTypes = types
+            .Where(p => typeof(VersionedSqlStorageElement).IsAssignableFrom(p))
+            .Where(p => p != typeof(VersionedSqlStorageElement));
+        foreach (var sqlStorageElementType in sqlStorageElementTypes)
+        {
+            var genericSqlRepositoryInterfaceType = typeof(IVersionedSqlRepository<>).MakeGenericType(sqlStorageElementType);
+            var genericSqlRepositoryImplementationType = typeof(VersionedSqlStorageElement).MakeGenericType(sqlStorageElementType);
             services.AddTransient(genericSqlRepositoryInterfaceType, genericSqlRepositoryImplementationType);
         }
 
